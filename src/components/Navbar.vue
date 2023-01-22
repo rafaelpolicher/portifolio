fa3f7b5a42bf6fd2773d576f65246ff49004c18d
rafaelpolicher <110497809+RafaelPolicher@users.noreply.github.com>
2023-01-22 10:00:05 -0300

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,28 @@
 <template>
     <nav>
         <ul>
-            <li><a href="#about">About</a></li>
-            <li><a href="#knowledge">Knowledge</a></li>
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">Contact</a></li>
+            <li><a href="#about">About
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </a></li>
+            <li><a href="#knowledge">Knowledge                         <span></span>
+                <span></span>
+                <span></span>
+                <span></span></a></li>
+            <li><a href="#">Projects
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </a></li>
+            <li><a href="#">Contact
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </a></li>
         </ul>
     </nav>
 </template>
@@ -33,15 +51,79 @@ li{
 a{
     font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
     color: #6C0094;
-    border: 1px solid black;
+    border: 1px solid transparent;
     border-bottom: 1px solid #0F9410;
     padding: .5rem;
     transition: .7s;
-    
+    position: relative;
 }
-a:hover{
+/*a:hover{
     color:#220794;
-    border: 1px solid green;
+    border: 1px solid #0F9410;
     border-radius: 5px;
+}*/
+
+a span{
+    position: absolute;
+    display: block;
+    background: #0F9410;
+}
+
+a span:nth-child(1){
+    left: 0;
+    top: 0;
+    width: 50.5%;
+    height: 1px;
+    transform: scaleX(0);
+    transform-origin: left;
+    transition: transform .1s;
+    transition-delay: .1s;
+}
+a:hover span:nth-child(1){
+  transform: scaleX(1);
+  transform-origin: left;
+}
+
+a span:nth-child(2){
+    right: 0;
+    top: 0;
+    width: 50.5%;
+    height: 1px;
+    transform: scaleX(0);
+    transform-origin: right;
+    transition: transform .1s;
+    transition-delay: .1s;
+}
+a:hover span:nth-child(2){
+  transform: scaleX(1);
+  transform-origin: right;
+}
+
+a span:nth-child(3){
+    right: 0;
+    top: 0;
+    width: 1px;
+    height: 100%;
+    transform: scaleY(0);
+    transform-origin: bottom;
+    transition: transform .1s;
+}
+a:hover span:nth-child(3){
+  transform: scaleX(1);
+  transform-origin: bottom;
+}
+
+a span:nth-child(4){
+    left: 0;
+    top: 0;
+    width: 1px;
+    height: 100%;
+    transform: scaleY(0);
+    transform-origin: bottom;
+    transition: transform .1s;
+}
+a:hover span:nth-child(4){
+  transform: scaleX(1);
+  transform-origin: bottom;
 }
 </style>
