fa2532536eb1c4c1bca82c4bdbd7b7067db04a37
rafaelpolicher <110497809+RafaelPolicher@users.noreply.github.com>
2023-02-22 21:05:06 -0300

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,7 @@
     <section id="about" v-motion-slide-visible-left>
         <h2>About</h2>
         <p>Passionate about technology and a beginner in web development.
- I had my first contact with development in a tech course in 2011. In 2016 I entry in Military Police, and now, after 7 years I'm changing careers.
+ I had my first contact with development in a tech course in 2011. In 2016 I entry in Military Police, and now, where I used C, C++, C#, and Java, now after 7 years I'm changing careers.
 Years of experience in a military environment brought several soft skills, such as interpersonal relationships, being a team player, communication, resilience, and the ability to work under pressure.
 I'm self-taught in programming, studying through documentation and courses like <a href="../../public/certificate.pdf" download="../../public/certificate.pdf">Web Design for Everybody: Basics of Web Development & Coding at the University of Michigan ⇩.</a>
  As a beginner, I'm willing to learn other technologies and ready to take a step forward working in a team of developers.
